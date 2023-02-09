48a8f98c518ac58a253a79e21c37ee3204f620b7
alexey.rotar <ar_mail@inbox.ru>
2023-02-10 02:28:39 +0500

    v1

---
+++ Task_58/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+
+// Генерация массива
+int[,] GeneratingArray2D(uint lines, uint columns)
+{
+    Random rnd = new Random();
+    int[,] array = new int[lines, columns];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = rnd.Next(0, 10);
+    }
+    return array;
+}
+
+// Перемножение матриц
+int[,] MultiplicationOfArrays(int[,] arrayA, int[,] arrayB)
+{
+    if (arrayA.GetLength(1) == arrayB.GetLength(0))
+    {
+        int[,] arrayC = new int[arrayA.GetLength(0), arrayB.GetLength(1)];
+        for (int i = 0; i < arrayA.GetLength(0); i++)
+        {
+            for (int j = 0; j < arrayB.GetLength(1); j++)
+            {
+                arrayC[i, j] = 0;
+                for (int k = 0; k < arrayA.GetLength(1); k++)
+                    arrayC[i, j] += arrayA[i, k] * arrayB[k, j];
+            }
+        }
+        return arrayC;
+    }
+    else
+    {
+        int[,] arrayC = new int[1, 1];
+        return arrayC;
+    }
+}
+
+// Вывод массива
+void OutputingArray2D(int[,] array, string message)
+{
+    Console.WriteLine(message);
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i, j] + "\t");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] arrayA = GeneratingArray2D(5, 3);
+int[,] arrayB = GeneratingArray2D(3, 4);
+OutputingArray2D(arrayA, "Матрица A: ");
+OutputingArray2D(arrayB, "Матрица B: ");
+int[,] arrayC = MultiplicationOfArrays(arrayA, arrayB);
+OutputingArray2D(arrayC, "Произведение матриц A и B: ");
---
+++ Task_62/Program.cs
@@ -0,0 +1,79 @@
+﻿// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4. 
+
+// Генерация пустого массива со случайным количеством строк [3...10] и столбцов [3...10]
+int[,] GeneratingArray2D()
+{
+    Random rnd = new Random();
+    int[,] array = new int[rnd.Next(3, 11), rnd.Next(3, 11)];
+    return array;
+}
+
+// Заполнение массива по спирали
+int[,] SpiralArray2DFilling(int[,] array)
+{      
+    int horizontalPass = 0,     // счетчик горизонтальных проходов
+        verticalPass = 0,       // счетчик вертикальных проходов
+        linePosition = 0,       // текущая позиция по строке
+        columnPosition = 0,     // текущая позиция по столбцу           
+        right = 0,              // счетчик шагов вправо
+        down = 0,               // счетчик шагов вниз
+        left = 0,               // счетчик шагов влево
+        up = 0,                 // счетчик шагов вверх
+        value = 1;              // значение
+   
+    while (array.GetLength(0) > horizontalPass && array.GetLength(1) > verticalPass) // пока есть куда двигаться
+    {
+        for (right = 0; right < array.GetLength(1) - verticalPass && value <= (array.GetLength(0) * array.GetLength(1)); ++right)
+        {
+            array[linePosition, columnPosition + right] = value;
+            value++;
+        }
+        linePosition++;
+        columnPosition = columnPosition + right - 1;
+        horizontalPass++;
+
+        for (down = 0; down < array.GetLength(0) - horizontalPass && value <= (array.GetLength(0) * array.GetLength(1)); ++down)
+        {
+            array[linePosition + down, columnPosition] = value;
+            value++;
+        }
+        linePosition = linePosition + down - 1;
+        columnPosition--;
+        verticalPass++;
+
+        for (left = 0; left < array.GetLength(1) - verticalPass && value <= (array.GetLength(0) * array.GetLength(1)); ++left)
+        {
+            array[linePosition, columnPosition - left] = value;
+            value++;
+        }
+        linePosition--;
+        columnPosition = columnPosition - left + 1;
+        horizontalPass++;
+
+        for (up = 0; up < array.GetLength(0) - horizontalPass && value <= (array.GetLength(0) * array.GetLength(1)); ++up)
+        {
+            array[linePosition - up, columnPosition] = value;
+            value++;
+        }
+        linePosition = linePosition - up + 1;
+        columnPosition++;
+        verticalPass++;
+    }
+    return array;
+}
+
+// Вывод массива
+void OutputingArray2D(int[,] array, string message)
+{
+    Console.WriteLine(message);
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i, j] + "\t");
+        Console.WriteLine();
+    }
+}
+
+int[,] array = GeneratingArray2D();
+OutputingArray2D(array, "Пустой массив: ");
+OutputingArray2D(SpiralArray2DFilling(array), "Заполненный массив: ");
